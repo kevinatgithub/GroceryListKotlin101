78a4d9a14d670a7287bbdf8a1f85d670f8a4464e
Kevin <933595@cognizant.com>
2023-08-24 16:05:05 +0800

    more fix

--- app/app/src/main/java/com/kotlin101/group2/grocerylist/adapters/ItemListAdapter.kt
+++ app/app/src/main/java/com/kotlin101/group2/grocerylist/adapters/ItemListAdapter.kt
@@ -6,11 +6,14 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import com.kotlin101.group2.grocerylist.GroceryAppHelpers
+import com.kotlin101.group2.grocerylist.GroceryAppHelpers.applyText
 import com.kotlin101.group2.grocerylist.R
 import com.kotlin101.group2.grocerylist.data.api.models.Item
 import com.squareup.picasso.Picasso
 
-class ItemListAdapter(private val items: List<Item>) : RecyclerView.Adapter<ItemViewHolder>() {
+class ItemListAdapter(private val items: List<Item>, private val callback: (id:Int) -> Unit) : RecyclerView.Adapter<ItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_card, parent, false)
@@ -25,19 +28,23 @@ class ItemListAdapter(private val items: List<Item>) : RecyclerView.Adapter<Item
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = items[position]
         holder.apply {
-            if (item.imgUrl != null){
+            card.setOnClickListener {
+                callback(item.id)
+            }
+            if (item.imgUrl != null && !item.imgUrl.toString().isEmpty()){
                 Picasso.get().load(item.imgUrl!!.toString()).into(ivImg)
             }else{
                 // base64 to image
             }
-            tvName.text = item.name
-            tvDesc1.text = item.description
-            tvDesc2.text = "P ${item.totalPrice} | P ${item.pricePerUnit} per unit x ${item.quantity} pcs"
+            tvName.text = applyText(item.name)
+            tvDesc1.text = applyText(item.description)
+            tvDesc2.text = "P ${applyText(item.totalPrice)} | P ${applyText(item.pricePerUnit)} per unit x ${applyText(item.quantity)} pcs"
         }
     }
 }
 
 class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val card: MaterialCardView = itemView.findViewById(R.id.card)
     val ivImg: ImageView = itemView.findViewById(R.id.iv_item_image)
     val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
     val tvDesc1: TextView = itemView.findViewById(R.id.tv_item_description_1)
--- app/app/src/main/java/com/kotlin101/group2/grocerylist/data/api/models/SyncRequest.kt
+++ app/app/src/main/java/com/kotlin101/group2/grocerylist/data/api/models/SyncRequest.kt
@@ -1,3 +1,3 @@
 package com.kotlin101.group2.grocerylist.data.api.models
 
-data class SyncRequest ( val items : ArrayList<Item>)
+data class SyncRequest ( val items : List<Item>)
